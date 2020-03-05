5ff22852f669b8e8cc4aebb8b86f248e5aa7b046
Bouloute <annaick1995@gmail.com>
2020-03-05 17:58:07 -0500

    refactoring

--- lib/CLI_Project/Player.rb
+++ lib/CLI_Project/Player.rb
@@ -1,20 +1,19 @@
 class Player
-    attr_reader :player_data, :name
+    attr_reader :player_data
     @@all = []
 
     def initialize(hash_of_data)
         @player_data = hash_of_data
-        @name = hash_of_data["Player"]
         @@all.push(self)
     end
 
 
     def self.display_all_names_only
-        @@all.each_with_index{|player, index| puts "#{index + 1}. #{player.name}"}
+        @@all.each_with_index{|player, index| puts "#{index + 1}. #{player.player_data["Player"]}"}
     end
 
     def display_all_stats
-        puts "Here is #{@name}'s stats:"
+        puts "Here is #{player_data["Team"]}'s stats:"
         player_data.each{|key, value|
             #added prettines
             case key.size
--- lib/CLI_Project/Team.rb
+++ lib/CLI_Project/Team.rb
@@ -1,20 +1,19 @@
 class Team
-    attr_reader :team_data, :name
+    attr_reader :team_data
     @@all = []
 
     def initialize(hash_of_data)
         @team_data = hash_of_data
-        @name = hash_of_data["Team"]
         @@all.push(self)
     end
 
 
     def self.display_all_names_only
-        @@all.each_with_index{|team, index| puts "#{index + 1}. #{team.name}"}
+        @@all.each_with_index{|team, index| puts "#{index + 1}. #{team.team_data["Team"]}"}
     end
 
     def display_all_stats
-        puts "Here is #{@name}'s stats:"
+        puts "Here is #{@team_data["Team"]}'s stats:"
         team_data.each{|key, value| 
             #added prettines
             case key.size
